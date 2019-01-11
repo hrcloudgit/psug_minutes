fc0d66f054c4317a57b0cecefc5bdc1838ce0623
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2019-01-11 01:16:26 -0500

    Create loops_with_arrays.ps1

---
+++ code/loops_with_arrays.ps1
@@ -0,0 +1,31 @@
+$servers = ('cm02','fs01','dc01')
+
+# using foreach...
+
+foreach ($server in $servers) {
+    Write-Host "checking $server"
+    # ...
+}
+
+# using pipeline iteration...
+
+$servers | ForEach-Object {
+    Write-Host "checking $_"
+    # ...
+}
+
+# using for...
+
+for ($i = 0; $i -lt $servers.Count; $i++) {
+    Write-Host "checking $($servers[$i])..."
+    # ...
+}
+
+# using while...
+
+$i = 0
+while ($i -lt $servers.Count) {
+    Write-Host "checking $($servers[$i])..."
+    #...
+    $i++
+}
